21c8e3c337f7ac2b4572f7551c850942aca1c35c
Rolf Veinø Sørensen <107861690+SteamDeckWindows@users.noreply.github.com>
2022-06-22 23:22:41 +0200

    begin version control

--- src/App/MainWindow.xaml.cs
+++ src/App/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using System.Windows;
 using AutoUpdaterDotNET;
+using System.Reflection;
 
 namespace SteamDeckWindows
 {
@@ -14,12 +15,19 @@ namespace SteamDeckWindows
         {
             InitializeComponent();
             AutoUpdater.Start("https://steamdeckwindows.github.io/Steam-Deck-Windows/assets/updates/latest.xml");
+            GetVersion();
         }
 
         private async void DownLoadDrivers_Click(object sender, RoutedEventArgs e)
         {
             Task task = new DriverService().DownloadDrivers(ProgressBar, SubProgressBar, ProgressLabel, SubProgressLabel);
             await task;
+        }
+
+        private void GetVersion()
+        {
+            lblVersion.Content = "Version " + GetType().Assembly.GetName().Version;
+
         }
     }
 }
