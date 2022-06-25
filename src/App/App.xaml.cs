0724aaedff5e9d16cfdff359974afef79947ff49
Rolf Veinø Sørensen <107861690+SteamDeckWindows@users.noreply.github.com>
2022-06-25 11:27:46 +0200

    add Serilog

--- src/App/App.xaml.cs
+++ src/App/App.xaml.cs
@@ -1,17 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using SteamDeckWindows.Commands;
+using SteamDeckWindows.ViewModels;
+using System;
 using System.Windows;
+using System.Windows.Input;
 
 namespace SteamDeckWindows
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .UseSerilog((host, loggerConfiguration) =>
+                {
+                    loggerConfiguration.WriteTo.File($"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\log.txt", rollingInterval: RollingInterval.Day)
+                        .WriteTo.Debug()
+                        .MinimumLevel.Error()
+                        .MinimumLevel.Override("SteamDeckWindows.Commands", Serilog.Events.LogEventLevel.Debug);
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<ICommand, MakeSandwichCommand>();
+                    services.AddSingleton<MainViewModel>();
+
+                    services.AddSingleton<MainWindow>(s => new MainWindow()
+                    {
+                        DataContext = s.GetRequiredService<MainViewModel>()
+                    });
+                })
+                .Build();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host.Start();
+
+            MainWindow = _host.Services.GetRequiredService<MainWindow>();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await _host.StopAsync();
+            _host.Dispose();
+
+            base.OnExit(e);
+        }
     }
 }
---
+++ src/App/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Input;
+
+namespace SteamDeckWindows.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ICommand MakeSandwichCommand { get; }
+
+        public MainViewModel(ICommand makeSandwichCommand)
+        {
+            MakeSandwichCommand = makeSandwichCommand;
+        }
+    }
+}
